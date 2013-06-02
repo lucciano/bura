44f26b442ba388ee2f91e908f6b5ab6dd1ebdb60
Sergio Milardovich <smilardovich@frro.utn.edu.ar>
2013-06-02 15:37:36 -0300

    Fixed Groups Sentinel class name

--- apps/groups/controllers/sentinel.php
+++ apps/groups/controllers/sentinel.php
@@ -1,13 +1,16 @@
 <?php
 
-class Sentinel{
+class GroupsDaemon{
 	function __construct(){
 		
 	}
 
 	function checkSection(){
-		global $groups;
-		
+/*		global $groups,$current_group;
+		if(!$groups->hasPrivilegies('app_'.$current_app.'_main',$current_group)){
+			die(_("You have no privilegies to see this section."));
+		}
+*/
 	}
 }
 

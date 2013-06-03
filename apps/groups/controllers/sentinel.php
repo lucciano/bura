2a51e233a918f3c62fb2c789fbca9c9f1593e189
Sergio Milardovich <smilardovich@frro.utn.edu.ar>
2013-06-03 16:15:18 -0300

    Generating default group tokens...

--- apps/groups/controllers/sentinel.php
+++ apps/groups/controllers/sentinel.php
@@ -2,15 +2,20 @@
 
 class GroupsDaemon{
 	function __construct(){
-		
+		global $security;
+		if(!isset($_SESSION['current_group'])){
+			$_SESSION['current_group'] = 0;
+			$token = $security->generateToken('group_token');
+			$this->addCurrentVisitor(0,$token);
+		}
 	}
 
 	function checkSection(){
-/*		global $groups,$current_group;
+		global $groups,$current_group;
 		if(!$groups->hasPrivilegies('app_'.$current_app.'_main',$current_group)){
 			die(_("You have no privilegies to see this section."));
 		}
-*/
+
 	}
 
 	function addCurrentVisitor($group,$token){
@@ -22,7 +27,10 @@ class GroupsDaemon{
 	}
 
 	// Tokens should be stored on the DB
-	function checkToken($token,$time,$ip=$_SERVER['REMOTE_ADDR']){
+	function checkToken($token,$time,$ip=null){
+		if(empty($ip)){
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
 
 	}
 
